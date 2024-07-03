228fed714871cb97eed58e47d1dedec969be3899
nicholas <njbroussard.1@gmail.com>
2024-07-03 17:05:53 -0500

    make Button_Display_Total : average_percentage

--- Part2/src/App.jsx
+++ Part2/src/App.jsx
@@ -9,56 +9,75 @@ const Display = (props) => {
           </div>
 }
 
+
 const Total = (props) => {
   return <div>
             <h2>Statistics</h2>
             <p>The toal number of votes is {props.total}</p>
+            <h2>Averages</h2>
+            <p>The average vote is: {props.average}</p>
+            <p>The positive vote rate is at {props.positive}%</p>
          </div>;
 }
 
+
 const Button = (props) => {
+  
   return <button onClick={props.handleClick}>
                 {props.text}
         </button>
 }
     
 
-
-
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [value, setValue] = useState(0);
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    const [total, setTotal] = useState([]);
+    const [average, setAverage] = useState(0);
+    const [positive, setPositive] = useState([]);
 
+    const handleClickGood = () => {
+      setGood(good + 1);
+      setTotal(total.concat(1));
+      setAverage(total.reduce((acc,curr)=>{
+          acc += curr;
+          return acc ? acc : 0;
+      },0) / total.length)
+      setPositive(positive.concat(1));
+      
+    }
 
-  const handleClickGood = () => {
-    setGood(good + 1);
-    setValue(value + 1);
-  }
+    const handleClickNeutral = () => {
+      setNeutral(neutral + 1);
+      setTotal(total.concat(0));
+      setAverage(total.reduce((acc,curr)=>{
+        acc += curr;
+        return acc ? acc : 0;
+      },) / total.length)
+    }
 
-  const handleClickNeutral = () => {
-    setNeutral(neutral + 1);
-    setValue(value + 1);
-  }
-
-  const handleClickBad = () => {
-    setBad(bad + 1);
-    setValue(value + 1)
-  }
+    const handleClickBad = () => {
+      setBad(bad + 1);
+      setTotal(total.concat(-1));
+      setAverage(total.reduce((acc,curr)=>{
+        acc += curr;
+        return acc ? acc : 0;
+      },0) / total.length)
+    }
 
   
-
   return (
     <div>
       <Button handleClick={handleClickGood} text='good' />
       <Button handleClick={handleClickNeutral} text='neutral' />
       <Button handleClick={handleClickBad} text='bad' />
-      <Display good={good} bad={bad} neutral={neutral}/>
-      <Total total={value}/>
+      <Display good={good} bad={bad} neutral={neutral} />
+      <Total total={total.length} average={average} positive={(positive.length / total.length).toFixed(2) * 100} />
     </div>
   )
+
 }
 
 
