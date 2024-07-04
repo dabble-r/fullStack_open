928b9bbac24d0620fa1d5630073117f4ede97a24
nicholas <njbroussard.1@gmail.com>
2024-07-04 16:05:14 -0500

    make table for data 1.10

--- Part2/src/App.jsx
+++ Part2/src/App.jsx
@@ -1,18 +1,14 @@
 import { useState } from 'react'
 
 
-
+/*
 const Display1 = () => {
   return <div>
             <h2>Statistics</h2>
             <p>No feedback given</p>
          </div>
 }
-
-
-
-
-
+*/
 
 
 const StatisticsLine = (props) => {
@@ -23,14 +19,59 @@ const StatisticsLine = (props) => {
 }
 
 
-
-
 const Button = (props) => {
   return <button onClick={props.handleClick}>
                 {props.text}
         </button>
 }
     
+
+const Table = (props) => {
+  return <table>
+            <thead>
+              <tr>
+                <th>Statistics</th>
+              </tr>
+              <tr>
+                <td>{props.text}</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Good</td>
+                <td>{props.good}</td>
+              </tr>
+              <tr>
+                <td>Bad</td>
+                <td>{props.bad}</td>
+              </tr>
+              <tr>
+                <td>Neutral</td>
+                <td>{props.neutral}</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>{props.total}</td>
+              </tr>
+              </tbody>
+            <tfoot>
+              <tr>
+                <th>Averages</th>
+              </tr>
+              <tr>
+                <td>Average vote</td>
+                <td>{props.average}</td>
+              </tr>
+              <tr>
+                <td>Positive rate</td>
+                <td>{props.positive}</td>
+              </tr>
+            </tfoot>
+        </table>
+}
+
+
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -75,7 +116,7 @@ const App = () => {
           <Button handleClick={handleClickGood} text='good' />
           <Button handleClick={handleClickNeutral} text='neutral' />
           <Button handleClick={handleClickBad} text='bad' />
-          <Display1 />
+          <Table text="No feedback given"/>
         </div>
       ) : total.length === 1
       ? 
@@ -84,11 +125,7 @@ const App = () => {
           <Button handleClick={handleClickGood} text='good' />
           <Button handleClick={handleClickNeutral} text='neutral' />
           <Button handleClick={handleClickBad} text='bad' />
-          <StatisticsLine text="Good" value={good} />
-          <StatisticsLine text="Neutral" value={neutral} />
-          <StatisticsLine text="Bad" value={bad} />
-          <StatisticsLine heading='Statistics' text="Total votes:" value={total.length} />
-          <StatisticsLine text="Positive rate: " value={(positive.length / total.length).toFixed(1) * 100 + '%'} />
+          <Table good={good} neutral={neutral} bad={bad} total={total.length} />
         </div>
       )
       :
@@ -97,18 +134,33 @@ const App = () => {
           <Button handleClick={handleClickGood} text='good' />
           <Button handleClick={handleClickNeutral} text='neutral' />
           <Button handleClick={handleClickBad} text='bad' />
-          <StatisticsLine text="Good" value={good} />
-          <StatisticsLine text="Neutral" value={neutral} />
-          <StatisticsLine text="Bad" value={bad} />
-          <StatisticsLine heading='Statistics' text="Total votes:" value={total.length} />
-          <StatisticsLine text="Average" value={average.toFixed(2)} />
-          <StatisticsLine text="Positive rate: " value={(positive.length / total.length).toFixed(1) * 100 + '%'} />
+          <Table good={good} neutral={neutral} bad={bad} total={total.length} average={average.toFixed(1)} positive={(positive.length / total.length).toFixed(1) * 100 + '%'}/>
         </div>
       )
 }
 
 
+//Display some values
+/*
+<StatisticsLine text="Good" value={good} />
+          <StatisticsLine text="Neutral" value={neutral} />
+          <StatisticsLine text="Bad" value={bad} />
+          <StatisticsLine heading='Statistics' text="Total votes:" value={total.length} />
+          <StatisticsLine text="Positive rate: " value={(positive.length / total.length).toFixed(1) * 100 + '%'} />
 
+
+*/
+
+
+// Display full values
+/*
+          <StatisticsLine text="Good" value={good} />
+          <StatisticsLine text="Neutral" value={neutral} />
+          <StatisticsLine text="Bad" value={bad} />
+          <StatisticsLine heading='Statistics' text="Total votes:" value={total.length} />
+          <StatisticsLine text="Average" value={average.toFixed(1)} />
+          <StatisticsLine text="Positive rate: " value={(positive.length / total.length).toFixed(1) * 100 + '%'} />
+*/
 
 
 export default App
