236fae4228343178e445f10928d3f02a5bea8fdc
nicholas <njbroussard.1@gmail.com>
2024-07-10 13:35:21 -0500

    make display_arbitrary num of courses

--- Part2/src/App.jsx
+++ Part2/src/App.jsx
@@ -14,61 +14,119 @@ const Course = (props) => {
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
+  const courses = [
       {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  const arrMap = courses.map(ele => {
+    let name;
+    let exercises;
+    let id;
+    
+
+    const parts = ele['parts'].map(item => {
+      return <ul>
+              <li key={item['id']}>Part: {item['name']} {item['exercises']}</li>
+            </ul>
+    })
+         
+        
+      
+
+    
+
+    return <div key={ele['id']}>
+              <h2>{ele['name']}</h2>
+              <span>Number of Parts: {ele['parts'].length}</span>
+              <p>Parts</p>
+                {parts}
+           </div>
+  })
+  
+
+
+  return <div>
+          {arrMap}
+        </div>
+  
+  /*
+  let name = [];
+  let id = [];
+  let len = [];
+  let parts = [];
+  let exercises = [];
+  let total = [];
+  */
+
+  /*
+  // last array obj overwrites all preceding, not a solution      
+  // for array data structure (courses)
+  for (let i = 0; i < courses.length; i++) {
+    for (let key in courses[i]) {
+      if (key === 'name') {
+        name.push(courses[i][key]);
       }
-    ]
+      if (key === 'id') {
+        id.push(courses[i][key]);
+      }
+      if (key === 'parts') {
+        len.push(courses[i][key].length);
+        parts.push(courses[i][key].map(ele => {
+          return <li key ={ele.id}>part name: `{ele.name}` -- exercises: {ele.exercises} </li>
+        }))
+        total.push(courses[i][key].reduce((acc,curr) => {
+            acc += curr.exercises;
+            return acc;
+        },0))
+      }
+    }
   }
-
-  let name;
-  let id;
-  let len;
-  let parts;
-  let exercises;
-  let total;
-
-  for (let key in course) {
-    if (key === 'name') {
-      name = course[key];
-    }
-    if (key === 'id') {
-      id = course[key];
-    }
-    if (key === 'parts') {
-      len = course[key].length;
-      parts = course[key].map(ele => {
-        return <li key={ele.id}>part name: '{ele.name}' -- exercises: {ele.exercises}</li>
-      })
-      total = course[key].reduce((acc,curr) => {
-        acc += curr.exercises;
-        return acc;
-      },0);
-    }
-
-  }
-
-  return <Course course={name} id={`course id: #${id} -- course length: ${len} parts`} parts={parts} exercises={exercises} total={total}/>
+*/
+  
+  //return <Course course={name} id={`course id: #${id} -- course length: ${len} parts`} parts={parts} exercises={exercises} total={total}/>
+    
+  
 }
 
 export default App
