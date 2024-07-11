9082164a179b4f6f180a76854d279d3a62b70034
nicholas <njbroussard.1@gmail.com>
2024-07-11 16:24:04 -0500

    make Number handler/add functions_display 2.8

--- Part2.6_2.10/my-react-app/src/App.jsx
+++ Part2.6_2.10/my-react-app/src/App.jsx
@@ -1,53 +1,64 @@
 import { useState } from 'react'
 import Name from './components/Name'
+import Number from './components/Number'
 
 function App() {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', id: 1}
-  ])
-  const [newName, setName] = useState('')
-
-  const handleNewName = (event) => {
-    for (let i = 0; i < persons.length; i++) {
-      if (persons[i].name === event.target.value) {
-        window.alert(`${event.target.value} is already added ot the phonebook.`)
-        setName('enter name here')
-      } else {
-        setName(event.target.value);
-      }
-    }
-  } 
-
-  const addName = (event) => {
+    {name: 'Arto Hellas', id: 1, number:123456789}
+  ]);
+  const [newName, setName] = useState('');
+  const [newNumber, setNumber] = useState(`___-___-___`);
+  
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const nameObj = {
+    const personObj = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
-    setPersons(persons.concat(nameObj))
-    setName('enter name here')
-    console.log(persons)
+        setPersons(persons.concat(personObj))
+        console.log(persons)
+        setName('enter name here')
+        setNumber('___-___-___')
   }
 
+const handleNewName = (event) => {
+  for (let i = 0; i < persons.length; i++) {
+    if (persons[i].name === event.target.value) {
+      window.alert(`${event.target.value} is already added ot the phonebook.`)
+      setName('enter name here')
+    } else {
+      setName(event.target.value);
+    }
+  }
+} 
+
+const handleNewNumber = (event) => {
+  setNumber(parseInt(event.target.value))
+  console.log(typeof newNumber)
+}
+ 
   return <div>
              <h2>Phonebook</h2>
-              <form onSubmit={addName}>
+              <form onSubmit={handleSubmit}>
                   <div>
                     name: <input value={newName} onChange={handleNewName}/>
                   </div>
-                  <div> debug: {newName}</div>
+                  <div> 
+                    number: <input value={newNumber} onChange={handleNewNumber}/>
+                  </div>
                   <div>
                     <button type='submit'>add</button>
                   </div>
               </form>
-              <ul>
-                {persons.map(ele => 
-                  <Name key={ele.id} name={ele.name} />
-                )}
-              </ul>
              <h2>Numbers</h2>
+             <ul>
+                {persons.map(ele => 
+                  <Number key={ele.id} name={ele.name} number={ele.number}/>
+                )}
+             </ul>
          </div>
-}
+  }
 
 
 
---
+++ Part2.6_2.10/my-react-app/src/components/Number.jsx
@@ -0,0 +1,13 @@
+
+
+const Number = (props) => {
+  return <li>{props.name} -- {props.number}</li>
+           
+}
+
+
+
+
+
+
+export default Number
